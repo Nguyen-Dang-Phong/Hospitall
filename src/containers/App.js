d3330c192dd2f5d31d743d6f6e55d973bc03f0c7
Nguyen-Dang-Phong <nguyendangphong0099@gmail.com>
2023-08-28 01:17:44 +0700

    Tích Hợp Thư Viện React-Slider-Carousel Trong React App
    npm install --save react-slick@0.28.1
    npm install --save slick-carousel@1.8.1

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,6 +18,7 @@ import System from '../routes/System';
 import { CustomToastCloseButton } from '../components/CustomToast';
 import HomePage from '../HomePage/HomePage';
 import ConfirmModal from '../components/ConfirmModal';
+import CustomScrollbars from '../components/CustomScrollbars';
 
 class App extends Component {
 
@@ -48,15 +49,17 @@ class App extends Component {
                         {/* Kiem tra xem neu da login thi vao trang headder */}
                         {this.props.isLoggedIn && <Header />}
 
-                        <span className="content-container">
-                            <Switch>
-                                <Route path={path.HOME} exact component={(Home)} />
-                                <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
-                                <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
-                                <Route path={path.HOMEPAGE} component={HomePage} />
+                        <div className="content-container">
+                            <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
+                                <Switch>
+                                    <Route path={path.HOME} exact component={(Home)} />
+                                    <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
+                                    <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                    <Route path={path.HOMEPAGE} component={HomePage} />
 
-                            </Switch>
-                        </span>
+                                </Switch>
+                            </CustomScrollbars>
+                        </div>
 
                         <ToastContainer
                             className="toast-container" toastClassName="toast-item" bodyClassName="toast-item-body"
